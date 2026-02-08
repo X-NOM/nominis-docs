c7792c10a55fd7b79a8f748502b609acd73b73b2
yotamb-nominis <yotam.b@nominis.io>
2026-02-08 11:52:38 +0200

    Update favicon to new_logo.ico

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Nominis API',
   tagline: 'Intelligent crypto compliance. Faster insights, smarter decisions.',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/new_logo.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
